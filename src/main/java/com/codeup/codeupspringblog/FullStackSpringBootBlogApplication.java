b5e9538870eb63e4c213a064573916fb9d5a8f6a
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-03-23 10:30:57 -0500

    Coursework

---
+++ src/main/java/com/codeup/codeupspringblog/FullStackSpringBootBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackSpringBootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackSpringBootBlogApplication.class, args);
+	}
+
+}
