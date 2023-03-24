f6277678775f188bad2a4eca825e6af3e24115e1
Joseph <118475783+josephmccomas@users.noreply.github.com>
2023-03-24 16:12:56 -0500

    Coursework

--- src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -1,22 +1,19 @@
 package com.codeup.codeupspringblog.controllers;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 public class RollDiceController {
 
     @GetMapping("/roll-dice/n")
-    public Integer diceGuess(@PathVariable Integer n, Model model) {
+    public Integer diceGuess(@RequestParam Integer n, Model model) {
         model.addAttribute("n", n);
         return n;
     }
 
     @PostMapping("/roll-dice")
     @ResponseBody
-    public Integer diceResult(@PathVariable Integer rando, Model model){
+    public Integer diceResult(@RequestParam Integer rando, Model model){
         model.addAttribute(Math.random() * 6);
         return rando;
     }
